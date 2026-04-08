7a0ca3a3ad46ee69d2f81cbddbfd2a572ecc519b
Julian <juliangarciapetcov16@gmail.com>
2026-04-08 16:45:38 +0200

    Tercer Push

--- src/main/java/Entidades/AnalizadorPartidas.java
+++ src/main/java/Entidades/AnalizadorPartidas.java
@@ -2,6 +2,7 @@ package Entidades;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -10,15 +11,15 @@ public class AnalizadorPartidas {
     List<Partida> partidas = new ArrayList<>();
 
     partidas.add(new Partida(1, "Juan_Lopez", "CPU_Madrid", ModoJuego.PVP, "FC_Barcelona",
-            3, 1, "12 Min", LocalDateTime.of(2026,4,
+            3, 1, "12min", LocalDateTime.of(2026,4,
             1,19,30),120));
 
     partidas.add(new Partida(2, "Maria_Garcia", "Rafael_Torres", ModoJuego.EVENTO, "Real_Madrid",
-                2, 2, "20 Min", LocalDateTime.of(2026,4,
+                2, 2, "20min", LocalDateTime.of(2026,4,
                 2,21,15),85));
 
     partidas.add(new Partida(3, "Carlos_Martin", "Equipo_IA", ModoJuego.LIGA, "Atletico_Madrid",
-                1, 0, "12 Min", LocalDateTime.of(2026,4,
+                1, 0, "12min", LocalDateTime.of(2026,4,
                 3,18,45),150));
 
     partidas.add(new Partida(4, "Lucia_Fernandez", "CPU_Sevilla", ModoJuego.PVP, "Sevilla_FC",
@@ -56,20 +57,44 @@ public class AnalizadorPartidas {
             .filter(p -> p.getGolesLocal() > p.getGolesVisitante())
             .forEach(System.out::println);
 
-    IO.println("---------------------");
+        IO.println("------------------------------");
     /* Consulta 2: Partidas por modo de juego*/
         //2.Contar cuántas partidas hay de cada modo (groupingBy).
     partidas.stream()
             .collect(Collectors.groupingBy(Partida::getModoJuego, Collectors.counting()))
             .forEach((c,v) -> IO.println(c + ": " + v + " Partidas"));
 
-    IO.println("---------------------");
+        IO.println("------------------------------");
     /* Consulta 3: Goles totales marcados*/
         //3.Suma de todos los golesLocal.
         int totalGoles = partidas.stream()
                 .mapToInt(p -> p.getGolesLocal())
                 .sum();
                 IO.println("Total de goles marcados por los equipos Locales: " + totalGoles);
+
+    /* Consulta 4: Mejor rival derrotado*/
+        //4.Partida con más estrellas contra cada rival, ordenada por estrellas.
+        IO.println("------------------------------");
+        partidas.stream()
+                .sorted(Comparator.comparing(Partida::getEstrellasGanadas).reversed())
+                .limit(1)
+                .forEach(System.out::println);
+
+    /* Consulta 5: Media de estrellas por modo*/
+        //5.Calcular averagingInt(estrellasGanadas) por modoJuego.
+        IO.println("------------------------------");
+        partidas.stream()
+                .collect(Collectors.groupingBy(Partida::getModoJuego,
+                                                Collectors.averagingInt(Partida::getEstrellasGanadas)))
+                .forEach((c,v) -> IO.println(c + ": " + v + " Media de estrellas por modo de Juego"));
+
+    /* Consulta 6: Partidas de más de 20 minutos*/
+        //6.Filtrar duracion que contenga "20min" o "30min".
+        IO.println("------------------------------");
+        partidas.stream()
+                .filter(p -> p.getDuracion().equals("20min") || p.getDuracion().equals("30min"))
+                .forEach(System.out::println);
+
 
 
     }
